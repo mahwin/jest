c661b1bb6d0a7a8cabe65ea716a6c442fa0cb14e
youseock <mahwin7085@gmail.com>
2023-04-27 16:34:55 +0900

    정적이 컴포넌트 테스팅 with 스냅샷

---
+++ react/my-app/src/App.js
@@ -0,0 +1,18 @@
+import Hello from "./component/Hello";
+import Timer from "./component/Timer";
+
+const user = {
+  name: "Mike",
+  age: 30,
+};
+
+function App() {
+  return (
+    <div className="App">
+      <Hello user={user} />
+      <Timer />
+    </div>
+  );
+}
+
+export default App;
